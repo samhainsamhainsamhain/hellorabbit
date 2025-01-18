3ef5dbe53b5984c257be16b960b2b4659e0787c8
samhainsamhainsamhain <rikalashnikov@gmail.com>
2025-01-18 11:55:13 +0300

    first commit

---
+++ Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+const string message = "Hello world!";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+Console.WriteLine(message);
+
+Console.WriteLine("press Enter to exit");
+Console.ReadLine();
