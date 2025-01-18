e2f0a9d1ca0325cef165c04841df6a7a2e139b3f
samhainsamhainsamhain <rikalashnikov@gmail.com>
2025-01-18 18:00:23 +0300

    add work queues, queue durability, persistence, and acks

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -2,25 +2,37 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 
+int count = 0;
+
 var factory = new ConnectionFactory {HostName = "localhost"};
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+await channel.QueueDeclareAsync(queue: "hello", durable: true, exclusive: false, autoDelete: false,
     arguments: null);
+
+await channel.BasicQosAsync(prefetchCount: 1, prefetchSize: 0, global: false);
 
 Console.WriteLine(" [*] Waiting for messages.");
 
 var consumer = new AsyncEventingBasicConsumer(channel);
-consumer.ReceivedAsync += (model, ea) =>
+consumer.ReceivedAsync += async (model, ea) =>
 {
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
-    Console.WriteLine("received message: ", message);
-    return Task.CompletedTask;
+    Console.WriteLine("received message: " + message);
+
+    int dots = message.Split('.').Length - 1;
+
+    await Task.Delay(dots * 100);
+
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+
+    count++;
+    Console.WriteLine("done " + count);
 };
 
-await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+await channel.BasicConsumeAsync("hello", autoAck: false, consumer: consumer);
 
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -5,13 +5,44 @@ var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+await channel.QueueDeclareAsync(queue: "hello", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-const string message = "Hello world!";
-var body = Encoding.UTF8.GetBytes(message);
+List<string> messages = GetMessages();
 
-await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
-Console.WriteLine(message);
+SendMessages(messages, channel);
 
 Console.WriteLine("press Enter to exit");
-Console.ReadLine();
+
+static List<string> GetMessages()
+{
+    var rndm = new Random();
+    List<string> messages = new List<string>();
+
+    for(var i = 0; i < 200; i++)
+    {
+        var msg = "message number " + i + "..";
+
+        for (var j = 0; j < rndm.Next(2, 10); j++)
+        {
+            msg += ".";
+        }
+
+        messages.Add(msg);
+    }
+
+    return messages;
+}
+
+static async void SendMessages(List<string> messages, IChannel channel)
+{
+    foreach(var message in messages)
+    {
+        var body = Encoding.UTF8.GetBytes(message);
+        var props = new BasicProperties
+        {
+            Persistent = true,
+        };
+        await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", mandatory: true, basicProperties: props, body: body);
+        Console.WriteLine(message);
+    }
+}
