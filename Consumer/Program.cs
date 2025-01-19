08d9b14a7ac7a8d23de3a7364beb2c8b17997baa
samhainsamhainsamhain <rikalashnikov@gmail.com>
2025-01-19 15:20:49 +0300

    add rpc

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,18 +1,9 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Reflection;
 using System.Text;
 
-if (args.Length < 1)
-{
-    Console.Error.WriteLine("Usage: {0} [binding_key...]",
-                            Environment.GetCommandLineArgs()[0]);
-    Console.WriteLine(" Press [enter] to exit.");
-    Console.ReadLine();
-    Environment.ExitCode = 1;
-    return;
-}
-
-Console.WriteLine("Used topic: {0}", args);
+const string QUEUE_NAME = "rpc_queue";
 
 int count = 1;
 
@@ -20,29 +11,65 @@ var factory = new ConnectionFactory {HostName = "localhost"};
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-await channel.ExchangeDeclareAsync(exchange: "topic_logs", type: ExchangeType.Topic);
+await channel.QueueDeclareAsync(queue: QUEUE_NAME, durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-QueueDeclareOk queueDeclareResult  = await channel.QueueDeclareAsync();
-string queueName = queueDeclareResult.QueueName;
-
-foreach (string? bindingKey in args)
-{
-    await channel.QueueBindAsync(queue: queueName, exchange: "topic_logs", routingKey: bindingKey);
-}
-
-Console.WriteLine(" [*] Waiting for messages.");
+await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
 
 var consumer = new AsyncEventingBasicConsumer(channel);
-consumer.ReceivedAsync += (model, ea) =>
+consumer.ReceivedAsync += async (model, ea) =>
 {
+    AsyncEventingBasicConsumer cons = (AsyncEventingBasicConsumer)model;
+    IChannel ch = cons.Channel;
+    string response = string.Empty;
+
     byte[] body = ea.Body.ToArray();
-    var message = Encoding.UTF8.GetString(body);
-    var routingKey = ea.RoutingKey;
-    Console.WriteLine($" [x] Received '{routingKey}':'{message}' --- '{count++}'");
-    return Task.CompletedTask;
+    IReadOnlyBasicProperties props = ea.BasicProperties;
+    var replyProps = new BasicProperties
+    {
+        CorrelationId = props.CorrelationId
+    };
+
+    try
+    {
+        var message = Encoding.UTF8.GetString(body);
+        int n = int.Parse(message);
+        Console.WriteLine($" [.] Fib({message})");
+        response = Fib(n).ToString();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($" [.] {e.Message}");
+        response = string.Empty;
+    }
+    finally
+    {
+        var responseBytes = Encoding.UTF8.GetBytes(response);
+        await ch.BasicPublishAsync(exchange: string.Empty, routingKey: props.ReplyTo!, mandatory: true, basicProperties: replyProps, body: responseBytes);
+        await ch.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+    }
 };
 
-await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
-
+await channel.BasicConsumeAsync(QUEUE_NAME, false, consumer);
+Console.WriteLine(" [x] Awaiting RPC requests");
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
+
+// Assumes only valid positive integer input.
+// Don't expect this one to work for big numbers,
+// and it's probably the slowest recursive implementation possible.
+static int Fib(int n)
+{
+    if (n is 0 or 1)
+    {
+        return n;
+    }
+    
+    List<int> ints = new List<int> { 0, 1 };
+
+    for(var i = 1; i < n; i++)
+    {
+        ints.Add(ints[i] + ints[i - 1]);
+    }
+
+    return ints[n];
+}
